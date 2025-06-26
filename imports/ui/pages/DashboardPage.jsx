0b4818196b7ccbeab1a292ecfd7f57bea012e767
Matteo Demicheli <matteodem@protonmail.com>
2025-06-26 22:28:06 +0200

    Add icons

--- imports/ui/pages/DashboardPage.jsx
+++ imports/ui/pages/DashboardPage.jsx
@@ -5,6 +5,7 @@ import { Mistakes } from '/imports/api/mistakes';
 import { Button } from '../base/Button';
 import { FenChessboard } from '../FenChessboard';
 import { CHESS_MISTAKE_TAGS } from '../../api/mistakeTags';
+import { FaCheck, FaTimes } from 'react-icons/fa';
 
 Meteor.subscribe('mistakes.all');
 
@@ -45,19 +46,19 @@ export const DashboardPage = () => {
 
   if (!practicing) {
     return (
-      <div className="bg-white p-6 rounded-lg shadow-md">
+      <div className="bg-white  p-6 rounded-lg shadow-md">
         <h2 className="text-2xl font-semibold text-gray-800 mb-4">Mistakes Due for Review</h2>
         {dueMistakes.length > 0 && (
           <Button className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition-colors duration-200" onClick={() => startPractice(dueMistakes)} disabled={dueMistakes.length === 0}>
             Practice ({dueMistakes.length})
           </Button>
         )}
-        {dueMistakes.length === 0 && <p className="italic text-gray-600 mt-4">No mistakes due for review.</p>}
+        {dueMistakes.length === 0 && <p className="italic text-gray-600  mt-4">No mistakes due for review.</p>}
 
         {(dueMistakes.length === 0 && allMistakes.length > 0) && (
-          <div className="mt-8 pt-6 border-t border-gray-200">
+          <div className="mt-8 pt-6 border-t border-gray-200 ">
             <h2 className="text-xl font-semibold text-gray-800 mb-2">Overtrain</h2>
-            <p className="italic text-gray-600 mb-4">Practice all your mistakes, even those not due for review.</p>
+            <p className="italic text-gray-600  mb-4">Practice all your mistakes, even those not due for review.</p>
             <Button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors duration-200" onClick={() => startPractice(allMistakes)} disabled={allMistakes.length === 0}>
               Overtrain ({allMistakes.length})
             </Button>
@@ -69,9 +70,9 @@ export const DashboardPage = () => {
 
   if (mistakes.length === 0) {
     return (
-      <div className="bg-white p-6 rounded-lg shadow-md">
-        <p className="italic text-gray-600 mb-4">No mistakes to practice.</p>
-        <Button className="bg-gray-300 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-400 transition-colors duration-200" onClick={() => setPracticing(false)}>Back</Button>
+      <div className="bg-white  p-6 rounded-lg shadow-md">
+        <p className="italic text-gray-600  mb-4">No mistakes to practice.</p>
+        <Button className="bg-gray-300 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-400 :bg-gray-600 transition-colors duration-200" onClick={() => setPracticing(false)}>Back</Button>
       </div>
     );
   }
@@ -79,17 +80,17 @@ export const DashboardPage = () => {
   const mistake = mistakes[currentMistakeIndex];
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md">
+    <div className="bg-white  p-6 rounded-lg shadow-md">
       <div className="flex flex-col md:flex-row items-center md:items-start">
         <FenChessboard fen={mistake.fen} orientation={mistake.orientation} isLarge={true} />
 
         <div className="flex-grow mt-6 md:mt-0 md:ml-12 w-full">
           {!showMistake && (
             <div className="text-center md:text-left">
-              <span className="text-xl font-semibold text-gray-800">I got it:&nbsp;</span>
+              <span className="text-xl font-semibold text-gray-800 ">I know the answer&nbsp;</span>
               <div className="mt-4 space-x-4">
-                <Button className="bg-green-600 text-white px-6 py-3 rounded-md hover:bg-green-700 transition-colors duration-200 text-lg" onClick={() => onReview(mistake._id, true)}>Correct ✅</Button>
-                <Button className="bg-red-600 text-white px-6 py-3 rounded-md hover:bg-red-700 transition-colors duration-200 text-lg" onClick={() => onReview(mistake._id, false)}>Wrong ❌</Button>
+                <Button className="bg-green-600 text-white px-6 py-3 rounded-md hover:bg-green-700 transition-colors duration-200 text-lg" onClick={() => onReview(mistake._id, true)}><FaCheck className="inline-block mr-2" />Correct</Button>
+                <Button className="bg-red-600 text-white px-6 py-3 rounded-md hover:bg-red-700 transition-colors duration-200 text-lg" onClick={() => onReview(mistake._id, false)}><FaTimes className="inline-block mr-2" />Wrong</Button>
               </div>
             </div>
           )}
